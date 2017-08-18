b929b3e4d5805e22c47f1ad33c6bd61b2bf751be
Vladimir <siniavsky.vladimir@gmail.com>
2017-08-18 10:28:11 +0300

    Payment structure, closes #7

---
+++ Kaolin.Services.PassRzdRu.Parser/Structs/Layer5734.cs
@@ -0,0 +1,28 @@
+﻿namespace Kaolin.Services.PassRzdRu.Parser.Structs
+{
+    /// <summary>
+    /// Payment
+    /// </summary>
+    public class Layer5734
+    {
+        public string Result { get; set; }
+        public string Timestamp { get; set; }
+
+        public string Url { get; set; }
+        public PaymentOptions BankExit { get; set; }
+
+        public class PaymentOptions
+        {
+            public string Ok { get; set; }
+            public string Cancel { get; set; }
+            public string Decline { get; set; }
+        }
+
+        public class Request
+        {
+            public string Type { get; set; }
+            public string OrderId { get; set; }
+            public string ActorType { get; set; }
+        }
+    }
+}
