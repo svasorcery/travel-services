6facddef740a89a823cd66ddbcfbeabca070321a
Vladimir <siniavsky.vladimir@gmail.com>
2017-09-26 17:43:29 +0300

    Ticket template structure, closes #12

---
+++ Kaolin.Services.PassRzdRu.Parser/Structs/Layer5422.cs
@@ -0,0 +1,10 @@
+﻿namespace Kaolin.Services.PassRzdRu.Parser.Structs
+{
+    /// <summary>
+    /// Ticket template
+    /// </summary>
+    class Layer5422
+    {
+        public string Template { get; set; }
+    }
+}
