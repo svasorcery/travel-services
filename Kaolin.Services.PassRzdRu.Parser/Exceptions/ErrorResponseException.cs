7af506f95c323a43bcc66667b7f86f0a1e573b60
Vladimir <siniavsky.vladimir@gmail.com>
2017-10-05 17:56:37 +0300

    added parser basic Exceptions

---
+++ Kaolin.Services.PassRzdRu.Parser/Exceptions/ErrorResponseException.cs
@@ -0,0 +1,18 @@
+﻿namespace Kaolin.Services.PassRzdRu.Parser.Exceptions
+{
+    public class ErrorResponseException : ParserException
+    {
+        private readonly string _method;
+        private readonly string _url;
+
+        public string Method => _method;
+        public string Url => _url;
+
+        public ErrorResponseException(string message, string method, string url) 
+            : base(message)
+        {
+            _method = method;
+            _url = url;
+        }
+    }
+}
