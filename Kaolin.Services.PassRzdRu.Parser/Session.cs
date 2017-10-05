4f72d0b3cf685799afc5b7e2b004e58915ca3c5d
Vladimir <siniavsky.vladimir@gmail.com>
2017-10-05 17:56:18 +0300

    added parser Session class

---
+++ Kaolin.Services.PassRzdRu.Parser/Session.cs
@@ -0,0 +1,9 @@
+﻿namespace Kaolin.Services.PassRzdRu.Parser
+{
+    public class Session
+    {
+        public string LtpaToken2 { get; set; }
+        public string JSessionId { get; set; }
+        public string UserName { get; set; }
+    }
+}
