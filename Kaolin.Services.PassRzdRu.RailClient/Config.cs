56e9b26f13bb39ecb47c984b217b4894d470be13
Vladimir <siniavsky.vladimir@gmail.com>
2017-11-01 17:34:07 +0300

    added PassRzdRuRailClient with config

---
+++ Kaolin.Services.PassRzdRu.RailClient/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace Kaolin.Services.PassRzdRu.RailClient
+{
+    public class Config
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ Kaolin.Services.PassRzdRu.RailClient/PassRzdRuRailClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Extensions.Options;
+
+namespace Kaolin.Services.PassRzdRu.RailClient
+{
+    using Parser;
+
+    public class PassRzdRuRailClient
+    {
+        private readonly Config _config;
+        private readonly PassRzdRuClient _parser;
+
+        public PassRzdRuRailClient(IOptions<Config> optionsAccessor, PassRzdRuClient parser)
+        {
+            _config = optionsAccessor.Value;
+            _parser = parser;
+        }
+    }
+}
