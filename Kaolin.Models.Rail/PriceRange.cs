bea3d78391ee3af91fdaff340cad2df5eef66d67
Vladimir <siniavsky.vladimir@gmail.com>
2017-11-09 12:45:32 +0300

    PriceRange

---
+++ Kaolin.Models.Rail/PriceRange.cs
@@ -0,0 +1,16 @@
+﻿namespace Kaolin.Models.Rail
+{
+    public class PriceRange
+    {
+        public decimal Min { get; internal set; }
+        public decimal Max { get; internal set; }
+
+        public PriceRange(decimal min, decimal max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public override string ToString() => Min == Max ? $"[{Min}]" : $"[{Min}...{Max}]";
+    }
+}
---
+++ Kaolin.Services.PassRzdRu.RailClient/Internal/Converters/PriceConverter.cs
@@ -0,0 +1,17 @@
+﻿using Kaolin.Models.Rail;
+
+namespace Kaolin.Services.PassRzdRu.RailClient.Internal.Converters
+{
+    public static class PriceConverter
+    {
+        public static decimal ToDecimal(string value)
+        {
+            return decimal.Parse(value, System.Globalization.NumberFormatInfo.InvariantInfo);
+        }
+
+        public static PriceRange ToPriceRange(string min, string max)
+        {
+            return new PriceRange(ToDecimal(min), ToDecimal(max));
+        }
+    }
+}
