cce4a68bfe73d99fd34acf03e65a2ead3e745a7a
Vladimir <siniavsky.vladimir@gmail.com>
2017-11-09 12:39:02 +0300

    moved common train info from SearchTrains to separate class

--- Kaolin.Models.Rail/SearchTrains.cs
+++ Kaolin.Models.Rail/SearchTrains.cs
@@ -26,26 +26,8 @@ namespace Kaolin.Models.Rail
             public IEnumerable<Train> Trains { get; set; }
 
 
-            public class Train
+            public class Train : TrainInfo
             {
-                public int OptionRef { get; set; }
-                public string Name { get; set; }
-                public string Number { get; set; }
-                public string DisplayNumber { get; set; }
-                public TripEvent Depart { get; set; }
-                public TripEvent Arrive { get; set; }
-                public TripEvent ArriveLocal { get; set; }
-                public TripEvent RouteStart { get; set; }
-                public string RouteEndStation { get; set; }
-                public string TimezoneDifference { get; set; }
-                public TimeSpan TripDuration { get; set; }
-                public int? TripDistance { get; set; }
-                public string Carrier { get; set; }
-                public string Brand { get; set; }
-                public bool IsFirm { get; set; }
-                public bool HasElectronicRegistration { get; set; }
-                public bool HasDynamicPricing { get; set; }
-                public bool IsComponent { get; set; }
                 public IEnumerable<Car> Cars { get; set; }
             }
 
---
+++ Kaolin.Models.Rail/TrainInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Kaolin.Models.Rail
+{
+    public class TrainInfo
+    {
+        public int OptionRef { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string DisplayNumber { get; set; }
+        public TripEvent RouteStart { get; set; }
+        public string RouteEndStation { get; set; }
+        public TripEvent Depart { get; set; }
+        public TripEvent Arrive { get; set; }
+        public TripEvent ArriveLocal { get; set; }
+        public TimeSpan TripDuration { get; set; }
+        public string TimezoneDifference { get; set; }
+        public string Carrier { get; set; }
+        public string Brand { get; set; }
+        public bool BEntire { get; set; }
+        public bool IsFirm { get; set; }
+        public bool IsComponent { get; set; }
+        public bool HasElectronicRegistration { get; set; }
+        public bool HasDynamicPricing { get; set; }
+        public int? TripDistance { get; set; }
+    }
+}
