cb2f9677f5bb1e7f7a0e1799446dcd743720609a
Vladimir <siniavsky.vladimir@gmail.com>
2017-11-14 12:46:11 +0300

    added Person

---
+++ Kaolin.Models.Rail/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Kaolin.Models.Rail
+{
+    public class Person
+    {
+        public int Ref { get; private set; }
+        public Gender? Gender { get; private set; }
+        public string FirstName { get; private set; }
+        public string MiddleName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime? BirthDate { get; private set; }
+        public Passport Passport { get; private set; }
+
+        public Person(int @ref, Gender? gender, string firstName, string middleName, string lastName, DateTime? birthDate, Passport passport)
+        {
+            Ref = @ref;
+            Gender = gender;
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Passport = passport;
+        }
+    }
+
+    public enum Gender
+    {
+        FEMALE = 0,
+        MALE = 1
+    }
+}
