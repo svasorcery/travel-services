cac83f1114b89d8b67411ed28b1472d08e0c65eb
Vladimir Sinyavsky <siniavsky.vladimir@gmail.com>
2017-11-24 12:00:04 +0300

    added MongoDbSession-Store/Provider, closes #43

---
+++ Kaolin.Infrastructure.SessionStore/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace Kaolin.Infrastructure.SessionStore
+{
+    public class Config
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+        public string Collection { get; set; }
+    }
+}
---
+++ Kaolin.Infrastructure.SessionStore/MongoDbSessionStore.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Kaolin.Infrastructure.SessionStore
+{
+    internal class MongoDbSessionStore : ISessionStore
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        
+        public DateTime Ttl { get; set; }
+        
+        public Dictionary<string, BsonDocument> Data { get; protected set; }
+
+        public MongoDbSessionStore(TimeSpan ttl) : this(
+            ObjectId.GenerateNewId().ToString(), 
+            DateTime.UtcNow + ttl)
+        {
+        }
+
+        public MongoDbSessionStore(string id, DateTime ttl)
+        {
+            Id = id;
+            Ttl = ttl;
+            Data = new Dictionary<string, BsonDocument>();
+        }
+
+
+        public void Store<T>(string key, T value)
+        {
+            if (Data.ContainsKey(key))
+            {
+                Data[key] = value.ToBsonDocument();
+            }
+            else
+            {
+                Data.Add(key, value.ToBsonDocument());
+            }
+        }
+
+        public T Retrieve<T>(string key) 
+            => Data.ContainsKey(key) ? BsonSerializer.Deserialize<T>(Data[key]) : default(T);
+    }
+}
