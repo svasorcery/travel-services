83583d5473f8f69bbd913221e2e846179bcdefa7
Vladimir Sinyavsky <siniavsky.vladimir@gmail.com>
2017-11-29 12:14:12 +0300

    added '/api/stations' endpoint for stations searching, closes #67

---
+++ Kaolin.Api.PassRzdRu/Controllers/StationsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kaolin.Api.PassRzdRu.Controllers
+{
+    using Kaolin.Infrastructure.Database;
+
+    [Route("api/[controller]")]
+    public class StationsController : Controller
+    {
+        private readonly StationsDbContext _stations;
+
+        public StationsController(StationsDbContext stations)
+        {
+            _stations = stations;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery]string term)
+        {
+            return Ok(await _stations.SearchAsync(term));
+        }
+    }
+}
