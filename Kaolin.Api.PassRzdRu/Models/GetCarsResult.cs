6a4a55af264674567f15ee6f9c14771d05d221b7
Vladimir Sinyavsky <siniavsky.vladimir@gmail.com>
2017-12-05 17:49:29 +0300

    added 'api/trains' endpoint for trains, cars and seats searching, closes #79

---
+++ Kaolin.Api.PassRzdRu/Models/GetCarsResult.cs
@@ -0,0 +1,10 @@
+﻿using Kaolin.Models.Rail;
+using System.Collections.Generic;
+
+namespace Kaolin.Api.PassRzdRu.Models
+{
+    public class GetCarsResult
+    {
+        public IEnumerable<QueryCars.Result.Car> Cars { get; set; }
+    }
+}
---
+++ Kaolin.Api.PassRzdRu/Models/GetTrainsResult.cs
@@ -0,0 +1,11 @@
+﻿using Kaolin.Models.Rail;
+using System.Collections.Generic;
+
+namespace Kaolin.Api.PassRzdRu.Models
+{
+    public class GetTrainsResult
+    {
+        public string SessionId { get; set; }
+        public IEnumerable<QueryTrains.Result.Train> Trains { get; set; }
+    }
+}
