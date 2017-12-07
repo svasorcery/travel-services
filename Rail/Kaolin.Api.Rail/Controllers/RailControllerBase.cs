21ff909dbfc8614e180185f5cf9459f03a3c4304
Vladimir Sinyavsky <siniavsky.vladimir@gmail.com>
2017-12-07 16:22:59 +0300

    added 'api/reserve' endpoint for reserve create and cancel, closes #80

---
+++ Rail/Kaolin.Api.Rail/Controllers/RailControllerBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kaolin.Api.Rail.Controllers
+{
+    public class RailControllerBase : Controller
+    {
+        protected IActionResult SessionExpired()
+            => StatusCode(410, new { error = "Session expired" });
+    }
+}
