729364c2488785dea7743816c41b37a2c056e40f
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2017-12-26 18:48:09 +0300

    added TrainsController and query trains request

---
+++ Clients/HermesSpa/Controllers/RailAPI/TrainsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HermesSpa.Controllers.RailAPI
+{
+    using HermesSpa.Services;
+
+    [Route("api/[controller]")]
+    public class TrainsController : Controller
+    {
+        private readonly RailKaolinApiClient _rail;
+
+        public TrainsController(RailKaolinApiClient railClient)
+        {
+            _rail = railClient;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get(RailKaolinApiClient.TrainsListRequest request)
+        {
+            if (request == null)
+            {
+                return BadRequest(nameof(request));
+            }
+
+            return Ok(await _rail.QueryTrainsAsync(request));
+        }
+    }
+}
---
+++ Clients/HermesSpa/Services/RailKaolinApiClient.TrainsListRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HermesSpa.Services
+{
+    public partial class RailKaolinApiClient
+    {
+        public class TrainsListRequest
+        {
+            public string From { get; set; }
+            public string To { get; set; }
+            public DateTime DepartDate { get; set; }
+            public int? HourFrom { get; set; }
+            public int? HourTo { get; set; }
+        }
+    }
+}
