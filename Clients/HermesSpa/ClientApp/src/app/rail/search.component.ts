833b853889383893b6edb2e3fdde0b4657d9704a
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-04-01 21:54:10 +0300

    [!] App : Moved to SpaServices 2.0 and Angular 5.2.9, changed the app structure

---
+++ Clients/HermesSpa/ClientApp/src/app/rail/search.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { TrainsListRequest, RailStation } from './rail.models';
+import { RailService, RailStationsListSource } from './rail.service';
+
+@Component({
+    templateUrl: 'search.component.html'
+})
+export class RailSearchComponent {
+    searchParams: TrainsListRequest;
+    railStationsSource: RailStationsListSource;
+
+    constructor(private _rail: RailService) {
+        this.searchParams = this._rail.getSearch();
+        this.railStationsSource = this._rail.getRailStationsSource();
+    }
+
+    public search(): void {
+        if (!this.searchParams) return;
+        this._rail.setSearch(this.searchParams);
+        this._rail.gotoTrains();
+    }
+}
