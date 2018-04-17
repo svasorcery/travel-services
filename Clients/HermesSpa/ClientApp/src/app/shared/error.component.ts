5410eb494a8ed606c45d756c9c925e638fc7538a
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-04-17 21:54:19 +0300

    [*] ErrorComponent : changed to process ApiError

--- Clients/HermesSpa/ClientApp/src/app/shared/error.component.ts
+++ Clients/HermesSpa/ClientApp/src/app/shared/error.component.ts
@@ -1,26 +1,32 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { ApiError } from '../errors/errors.models';
+
 @Component({
     selector: 'error',
     template: `
-        <div *ngIf="active">
-            <div class="alert alert-danger" [class.alert-dismissible]="dismissible" role="alert">
-                <button *ngIf="dismissible" type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <i class="fa fa-warning fa-3x pull-left"></i>
-                <div class="text-center">
-                    <h4>{{ message }}</h4>
-                    <h5>{{ details }}</h5>
+        <div *ngIf="active" class="panel panel-danger">
+            <div class="alert alert-danger panel-body" [class.alert-dismissible]="dismissible" role="alert" style="margin-bottom:0">
+                <div class="col-md-1 pull-right">
+                    <button *ngIf="dismissible" (click)="active=!active" type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <i class="fa fa-close" style="padding:0 20px"></i>
+                    </button>
                 </div>
-            </div>
-            <div *ngIf="refreshable" class="text-center">
-                <a (click)="refresh()">
-                    <h4>
-                        <span class="fa fa-refresh"></span>
-                        Refresh page
-                    </h4>
-                </a>
+                <div class="col-md-1 text-center">
+                    <i class="fa fa-warning fa-5x" style="margin-top:20px 0"></i>
+                </div>
+                <div class="col-md-10 text-center">
+                    <h3 style="margin-top:0">{{ error.code }}: {{ error.message }}</h3>
+                    <h4>{{ error.detail }}</h4>
+                    <div *ngIf="refreshable">
+                        <a (click)="refresh()" style="cursor:pointer">
+                            <h4>
+                                <span class="fa fa-refresh"></span>
+                                Обновить страницу
+                            </h4>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     `
@@ -29,13 +35,11 @@ export class ErrorComponent {
     @Input() active: boolean;
     @Input() dismissible: boolean = false;
     @Input() refreshable: boolean = true;
-
-    @Input() message: string = `There was a problem processing your request.`;
-    @Input() details: string = `Try again in a few minutes.`;
+    @Input() error: ApiError;
 
     constructor() { }
 
     refresh() {
-        window.location.reload();
+        window.location.reload();  
     }
 }
