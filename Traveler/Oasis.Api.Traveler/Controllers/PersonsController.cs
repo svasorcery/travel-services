ba4c892570c439e5affe71cb99cd62ea8c51e983
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-11-27 12:23:21 +0300

    [+] PersonsController : created 'api/persons' endpoint

---
+++ Traveler/Oasis.Api.Traveler/Controllers/PersonsController.cs
@@ -0,0 +1,74 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Oasis.Api.Traveler.Controllers
+{
+    using Oasis.Api.Traveler.Abstractions;
+    using Oasis.Api.Traveler.Models;
+
+    [Route("api/[controller]")]
+    public class PersonsController : Controller
+    {
+        private readonly IPersonsRepository _persons;
+
+        public PersonsController(IPersonsRepository personsRepository)
+        {
+            _persons = personsRepository;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _persons.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _persons.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Person value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest($"Model {nameof(Person)} is null");
+            }
+
+            return Ok(await _persons.CreateAsync(value));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute]int id, [FromBody]Person value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest($"Model {nameof(Person)} is null");
+            }
+
+            if ((await _persons.GetByIdAsync(id)) == null)
+            {
+                return NotFound($"{nameof(Person)} with id = {id} was not found");
+            }
+
+            return Ok(await _persons.UpdateAsync(value));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute]int id)
+        {
+            var value = await _persons.GetByIdAsync(id);
+
+            if (value == null)
+            {
+                return NotFound($"{nameof(Person)} with id = {id} was not found");
+            }
+
+            await _persons.DeleteAsync(value);
+
+            return Ok();
+        }
+    }
+}
