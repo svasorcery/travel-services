1d3caedf3ac87fec82c7b0bb696cab07d18066f2
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-12-04 18:01:13 +0300

    [+] InMemoryContext : created InMemory database context and repositories

---
+++ Traveler/Oasis.Api.Traveler/Services/InMemory/InMemoryAccounrRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Oasis.Api.Traveler.Abstractions;
+using Oasis.Api.Traveler.Models;
+using Oasis.Api.Traveler.Services.InMemory;
+
+namespace Oasis.Api.Traveler.Services
+{
+    public class InMemoryAccountsRepository : ICatalogueRepository<Account>
+    {
+        private readonly InMemoryStorageContext _db;
+
+        public InMemoryAccountsRepository(InMemoryStorageContext context)
+        {
+            _db = context;
+        }
+
+
+        public Task<Account[]> GetAllAsync()
+        {
+            return Task.FromResult(_db.Accounts.ToArray());
+        }
+
+        public Task<Account> GetByIdAsync(int id)
+        {
+            var model = _db.Accounts.First(x => x.Id == id);
+            model.Person = _db.Persons.FirstOrDefault(p => p.Id == model.PersonId);
+
+            return Task.FromResult(model);
+        }
+
+        public Task<Account> CreateAsync(Account model)
+        {
+            model.Id = _db.Accounts.Count() > 0 ? _db.Accounts.Last().Id + 1 : 1;
+            model.CreatedAt = DateTime.UtcNow;
+            model.IsDeleted = false;
+
+            _db.Accounts.Add(model);
+
+            return Task.FromResult(model);
+        }
+
+        public Task<Account> UpdateAsync(Account model)
+        {
+            var update = _db.Accounts.First(x => x.Id == model.Id);
+
+            update.UserName = model.UserName;
+            update.Password = model.Password;
+            update.Email = model.Email;
+            update.MobilePhone = model.MobilePhone;
+            update.AvatarUri = model.AvatarUri;
+            update.LastUpdateAt = DateTime.UtcNow;
+
+            return Task.FromResult(update);
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            var model = _db.Accounts.First(x => x.Id == id);
+            model.IsDeleted = true;
+            return Task.FromResult(0);
+        }
+
+        public Task DeleteAsync(Account model)
+        {
+            model.IsDeleted = true;
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ Traveler/Oasis.Api.Traveler/Services/InMemory/InMemoryPersonsRepository.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Oasis.Api.Traveler.Abstractions;
+using Oasis.Api.Traveler.Models;
+using Oasis.Api.Traveler.Services.InMemory;
+
+namespace Oasis.Api.Traveler.Services
+{
+    public class InMemoryPersonsRepository : ICatalogueRepository<Person>
+    {
+        private readonly InMemoryStorageContext _db;
+
+        public InMemoryPersonsRepository(InMemoryStorageContext context)
+        {
+            _db = context;
+        }
+
+
+        public Task<Person[]> GetAllAsync()
+        {
+            return Task.FromResult(_db.Persons.ToArray());
+        }
+
+        public Task<Person> GetByIdAsync(int id)
+        {
+            var model = _db.Persons.First(x => x.Id == id);
+
+            return Task.FromResult(model);
+        }
+
+        public Task<Person> CreateAsync(Person model)
+        {
+            model.Id = _db.Persons.Count() > 0 ? _db.Persons.Last().Id + 1 : 1;
+            _db.Persons.Add(model);
+
+            return Task.FromResult(model);
+        }
+
+        public Task<Person> UpdateAsync(Person model)
+        {
+            var update = _db.Persons.First(x => x.Id == model.Id);
+
+            update.FirstName = model.FirstName;
+            update.MiddleName = model.MiddleName;
+            update.LastName = model.LastName;
+            update.BirthDate = model.BirthDate;
+            update.Gender = model.Gender;
+
+            return Task.FromResult(update);
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            var model = _db.Persons.First(x => x.Id == id);
+            _db.Persons.Remove(model);
+            return Task.FromResult(0);
+        }
+
+        public Task DeleteAsync(Person model)
+        {
+            _db.Persons.Remove(model);
+            return Task.FromResult(0);
+        }
+    }
+}
